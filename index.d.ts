14fa00dae6140a81aa7f751f5009ea902a27e220
STARTRACEX <startraceebr@foxmail.com>
2023-10-15 00:27:54 +0800

    Add run build

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function createLiteralFunc<T>(rawFunc: (str: string) => T): (strings: TemplateStringsArray, ...values: any[]) => T;
-export declare function createStringFunc<T>(rawFunc: (strings: TemplateStringsArray, ...values: any[]) => T): (str: string) => T;
