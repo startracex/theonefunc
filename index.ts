6509123858cd89a954d8cb51cddc416b5195b1c3
STARTRACEX <startraceebr@foxmail.com>
2024-02-18 03:41:17 +0800

    Add types

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
-export function createLiteralFunc<T>(rawFunc: (str: string) => T) {
+export type TemplateStringFunction<T> = (strings: TemplateStringsArray, ...values: any[]) => T;
+export type StringFunction<T> = (str: string) => T;
+
+export function createLiteralFunc<T>(rawFunc: StringFunction<T>): TemplateStringFunction<T> {
   return (strings: TemplateStringsArray, ...values: any[]): T => {
-    const arg = strings.reduce(
-      (prev, cur, index) => prev + cur + (values[index] || ""),
-      ""
-    );
+    const arg = strings.reduce((prev, cur, index) => prev + cur + (values[index] || ""), "");
     return rawFunc(arg);
   };
 }
 
-export function createStringFunc<T>(
-  rawFunc: (strings: TemplateStringsArray, ...values: any[]) => T
-): (str: string) => T {
+export function createStringFunc<T>(rawFunc: TemplateStringFunction<T>): StringFunction<T> {
   return (str: string) => {
     return rawFunc([str] as unknown as TemplateStringsArray);
   };
